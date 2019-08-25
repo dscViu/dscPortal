b171f84d6030cc1e08807542b0e7e11c3e268a63
brinbackthedog <brigbackthedog@gmail.com>
2019-08-25 14:52:35 -0700

    settings fix (cache issues)

--- dscPortal/settings.py
+++ dscPortal/settings.py
@@ -22,6 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = ')!8tts7md%7!#p$*t$*o)@s*i!$g1fru+55p5*ql-7f%+_b^gg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
+#DEBUG = True
 DEBUG = False
 
 ALLOWED_HOSTS = [
